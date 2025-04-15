ed753088b1c27406bd156e46fbf4da8269fd06e2
Gigomeni0 <joaoantonio.goncalves2003@gmail.com>
2025-04-15 20:36:16 -0300

    Estrutura básica montada

---
+++ src/test/java/com/robotrack2k/logitrack/LogitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotrack2k.logitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
